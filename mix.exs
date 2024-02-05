ce852324e770227990163ae1b929e14896661a4c
Gonzalo <456459+grzuy@users.noreply.github.com>
2024-02-05 15:46:17 -0300

    build(deps): support elixir 1.15

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Juggler.MixProject do
     [
       app: :juggler,
       version: "0.1.0",
-      elixir: "~> 1.16",
+      elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
