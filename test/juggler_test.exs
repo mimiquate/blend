3129fd759561e7af54f16d95c555acf7c2b05e9d
Gonzalo <456459+grzuy@users.noreply.github.com>
2024-02-05 15:44:35 -0300

    test: adds test coverage to Juggler.init

--- test/juggler_test.exs
+++ test/juggler_test.exs
@@ -2,7 +2,22 @@ defmodule JugglerTest do
   use ExUnit.Case
   doctest Juggler
 
+  @tmp_path Path.expand("../tmp", __DIR__)
+
+  setup do
+    File.mkdir_p!(@tmp_path)
+    on_exit(fn -> File.rm_rf(@tmp_path) end)
+    :ok
+  end
+
   test "greets the world" do
-    assert Juggler.hello() == :world
+    File.cd!(
+      @tmp_path,
+      fn ->
+        refute File.exists?("juggles.exs")
+        assert Juggler.init() == :ok
+        assert File.exists?("juggles.exs")
+      end
+    )
   end
 end
