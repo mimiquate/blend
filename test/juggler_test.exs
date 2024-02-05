d6956a775248b49b7a94971feee3e5cc29f054bf
Gonzalo <456459+grzuy@users.noreply.github.com>
2024-02-05 16:34:49 -0300

    test: adds test coverage juggler.get task

--- test/juggler_test.exs
+++ test/juggler_test.exs
@@ -20,4 +20,24 @@ defmodule JugglerTest do
       end
     )
   end
+
+  test "juggler.get task" do
+    File.cd!(
+      @tmp_path,
+      fn ->
+        File.write(
+          "juggles.exs",
+          """
+          %{
+            "jason-1-0": [{:jason, "~> 1.0"}]
+          }
+          """
+        )
+        Mix.Task.run("juggler.get")
+        File.exists?("juggler/jason-1-0.mix.lock")
+      end
+    )
+
+    assert true
+  end
 end
