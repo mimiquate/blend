1aed613aa0e845aec315006baf545fba20303ca5
Gonzalo <456459+grzuy@users.noreply.github.com>
2024-02-06 17:01:19 -0300

    feat: mix blend.update

---
+++ lib/mix/tasks/blend/update.ex
@@ -0,0 +1,18 @@
+defmodule Mix.Tasks.Blend.Update do
+  use Mix.Task
+
+  @shortdoc "Updates lockfiles from blend.exs"
+
+  @impl true
+  def run(args) do
+    Blend.blends()
+    |> Enum.map(fn {blend_id, _deps} ->
+      Blend.within(
+        blend_id,
+        fn ->
+          Mix.Task.rerun("deps.update", args)
+        end
+      )
+    end)
+  end
+end
