cd3578e0204153900204d8b173d695b72619e8a2
Gonzalo <456459+grzuy@users.noreply.github.com>
2024-02-27 13:07:40 -0300

    docs: prefer recommending underscored blend name

--- lib/blend/templates/blend.exs
+++ lib/blend/templates/blend.exs
@@ -1,5 +1,5 @@
 # Example for testing against 1.x and 2.x of some dependency
 # %{
-#   "dep-name-1-0": [{:dep_name, "~> 1.0"}],
-#   "dep-name-2-0": [{:dep_name, "~> 2.0"}],
+#   dep_name_1: [{:dep_name, "~> 1.0"}],
+#   dep_name_2: [{:dep_name, "~> 2.0"}],
 # }
